30c82ab56789b6a0c102fbf3b240e0b8119ab4c4
Sean Lawrence <lawrensm@gmail.com>
2014-04-12 15:01:29 -0400

    use unminified three.js so easier to debug

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 PATH="./node_modules/.bin:$PATH"
 rm -rf dist
 mkdir -p dist/chrome/video-js/
-uglifyjs libs/three.min.js libs/PointerLockControls.js libs/vr/vr.js libs/vr/OculusRiftControls.js libs/vr/OculusRiftEffect.js libs/make3d.js -o dist/chrome/bundle.js
+uglifyjs libs/three.js libs/PointerLockControls.js libs/vr/vr.js libs/vr/OculusRiftControls.js libs/vr/OculusRiftEffect.js libs/make3d.js -o dist/chrome/bundle.js
 rsync -aP ./src/chrome/* ./dist/chrome/
 cp libs/vr/vr.js dist/chrome
 cp -r libs/video-js-4.4.3/ dist/chrome/video-js/
--- src/firefox/run.sh
+++ src/firefox/run.sh
@@ -7,7 +7,10 @@ PATH="./node_modules/.bin:$PATH"
 #why is the temp file necessary for FF???!!!
 # bundle libs and copy to data folder
 echo "console.log(\"begin bundle.js\")" > temp.js
-uglifyjs temp.js libs/three.min.js libs/PointerLockControls.js libs/vr/vr.js libs/vr/OculusRiftControls.js libs/vr/OculusRiftEffect.js libs/make3d.js src/firefox/data/content.js -o src/firefox/data/bundle.js
+uglifyjs temp.js libs/three.js libs/PointerLockControls.js libs/vr/vr.js libs/vr/OculusRiftControls.js libs/vr/OculusRiftEffect.js libs/make3d.js src/firefox/data/content.js -o src/firefox/data/bundle.js
+
+#make unminifiied for debug purposes
+cat temp.js libs/three.js libs/PointerLockControls.js libs/vr/vr.js libs/vr/OculusRiftControls.js libs/vr/OculusRiftEffect.js libs/make3d.js src/firefox/data/content.js >> src/firefox/data/unmini-bundle.js
 rm temp.js
 
 cd -
